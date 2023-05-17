169ccb17605f85f6c7c4c0f6409890d3dfce95fa
wuyueqi <wuyueqi@changba.com>
2023-05-17 22:29:10 +0800

    写一个正常的

--- ni_zhi_lian_biao/main/main.go
+++ ni_zhi_lian_biao/main/main.go
@@ -3,12 +3,11 @@ import (
 	"fmt"
 	."github.com/isdamir/gotype"
 )
-
+//这是带头节点的 面试谁这么写啊。。。。
 func Reverse(node *LNode)  {
 	if node == nil || node.Next == nil{
 		return
 	}
-
 	var pre *LNode
 	var cur *LNode
 	next := node.Next
@@ -20,10 +19,24 @@ func Reverse(node *LNode)  {
 	}
 	node.Next =pre
 }
+//不带头节点的
+func reverseList(head *LNode) *LNode {
+	var prev *LNode
+	cur := head
+	for cur != nil {
+		next := cur.Next  //简单的记忆  脑残嫖娼 右上=左下
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
+
+
 func main()  {
 	head := &LNode{}
 	fmt.Println("逆序")
-	CreateNode(head,10)
+	CreateNode(head,4)
 	PrintNode("原本:" ,head)
 	Reverse(head)
 	PrintNode("现在 :" ,head)
